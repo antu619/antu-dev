127f04a307c9df050c25a503155ca2d67d301639
Antu Chandra Das <antu.das.619@gmail.com>
2024-06-07 15:32:18 +0600

    Basic project setup and added Navbar

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import About from "./components/About"
+import Banner from "./components/Banner"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+import Projects from "./components/Projects"
+import Skills from "./components/Skills"
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Banner/>
+      <Projects/>
+      <Skills/>
+      <About/>
+      <Contact/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
