6f411df69a925b800f2ea23c7a8f23be4b4bd768
Antu Chandra Das <antu.das.619@gmail.com>
2024-06-08 15:17:28 +0600

    Added skills section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,11 +8,11 @@ const About = () => {
             <h2 className="text-3xl mb-20">About Me</h2>
             <div className="hero">
             <div className="hero-content gap-10 lg:gap-40 flex-col lg:flex-row-reverse p-0">
-                <div className="w-2/3 lg:w-1/4">
+                <div className="w-2/3 lg:w-1/4 md:w-auto">
                     <img className='rounded-2xl border-4 border-[#1D4786]'  src={antu} alt='' />
                 </div>
                 <div className='lg:w-1/2'>
-                <h1 className="text-3xl font-bold">Antu Das</h1>
+                <h1 className="text-3xl font-bold">Antu Chandra Das</h1>
                <div>
                <p className="py-6">As a passionate MERN Stack developer, I have acquired a solid understanding of HTML, CSS, JavaScript, and React. I am dedicated to staying up-to-date with the latest industry trends and best practices to create innovative and efficient web solutions.</p>
                <p>On the back-end, I am proficient with Node.js and Express, which I have used to build robust server-side applications. Additionally, I have experience with MongoDB for database management, completing projects that integrate full-stack functionality. </p>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,67 @@
-
+import html from '../assets/html.png';
+import css from '../assets/css.png';
+import js from '../assets/js.png';
+import react from '../assets/react.png';
+import tailwind from '../assets/tailwind.png';
+import bootstrap from '../assets/bootstrap.png';
+import node from '../assets/node.png';
+import express from '../assets/express.png';
+import mongo from '../assets/mongodb.png';
+import firebase from '../assets/frirebase.png';
+import git from '../assets/git.png';
+import Marquee from "react-fast-marquee";
 
 const Skills = () => {
     return (
-        <div>
-            Skills
+        <div className="py-10 max-w-7xl mx-auto p-5">
+            <h3 className='text-3xl mt-10 mb-16 ml-4'>My Technical Skills</h3>
+            <Marquee pauseOnHover>
+            <div className='grid items-center justify-center bg-[#1D4786] w-28 p-2 ml-2'>
+                <img className='w-20' src={html} alt="logo" />
+                <h3 className='text-center'>HTML</h3>
+            </div>
+            <div className='grid items-center justify-center bg-[#1D4786] w-28 p-2 ml-2'>
+                <img className='w-20' src={css} alt="logo" />
+                <h3 className='text-center'>CSS</h3>
+            </div>
+            <div className='grid items-center justify-center bg-[#1D4786] w-28 p-2 ml-2'>
+                <img className='w-20' src={tailwind} alt="logo" />
+                <h3 className='text-center'>Tailwind CSS</h3>
+            </div>
+            <div className='grid items-center justify-center bg-[#1D4786] w-28 p-2 ml-2'>
+                <img className='w-20' src={bootstrap} alt="logo" />
+                <h3 className='text-center'>Bootstrap</h3>
+            </div>
+            <div className='grid items-center justify-center bg-[#1D4786] w-28 p-2 ml-2'>
+                <img className='w-20' src={js} alt="logo" />
+                <h3 className='text-center'>JavaScript</h3>
+            </div>
+            <div className='grid items-center justify-center bg-[#1D4786] w-28 p-2 ml-2'>
+                <img className='w-20' src={react} alt="logo" />
+                <h3 className='text-center'>React.js</h3>
+            </div>
+            <div className='grid items-center justify-center bg-[#1D4786] w-28 p-2 ml-2'>
+                <img className='w-20' src={node} alt="logo" />
+                <h3 className='text-center'>Node.js</h3>
+            </div>
+            <div className='grid items-center justify-center bg-[#1D4786] w-28 p-2 ml-2'>
+                <img className='w-20' src={express} alt="logo" />
+                <h3 className='card-action text-center'>Express.js</h3>
+            </div>
+            <div className='grid items-center justify-center bg-[#1D4786] w-28 p-2 ml-2'>
+                <img className='w-20' src={mongo} alt="logo" />
+                <h3 className='text-center'>MongoDB</h3>
+            </div>
+            <div className='grid items-center justify-center bg-[#1D4786] w-28 p-2 ml-2'>
+                <img className='w-20' src={firebase} alt="logo" />
+                <h3 className='text-center'>Firebase</h3>
+            </div>
+            <div className='grid items-center justify-center bg-[#1D4786] w-28 p-2 ml-2'>
+                <img className='w-20' src={git} alt="logo" />
+                <h3 className='text-center'>Git</h3>
+            </div>
+            
+            </Marquee>
         </div>
     );
 };
